bb2d155a7cd84edbe597025ed297e08400420555
Deluxo <110329381+SergeantMacNuggets@users.noreply.github.com>
2026-03-25 23:22:29 +0800

    partially finish front end design

---
+++ src/App.jsx
@@ -0,0 +1,297 @@
+import React, { useEffect,useState } from 'react';
+import ReactDOM from "react-dom";
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay';
+import { NextButton } from './CarouselButtons';
+import Modal from 'react-modal';
+
+
+function MiniDisplay() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+          <div class="bg-indigo-950 w-100 h-40 border border-blue-400 rounded-xl grid grid-cols-2">
+            <div class = "min-h-50 max-h-50 min-w-50 max-w-50 p-5">
+              <div class = "grid grid-rows-2">
+                <div>
+                {ImageDisplay(false, <img src='/src/assets/img1.jpg'></img>)}
+                </div>
+
+                <div class="font-thin mt-2">
+                  <p>Current Bid:</p>
+                  <p class="font-bold text-lime-400">$10,240,000.00</p>
+                </div>
+              </div>
+               
+            </div>
+
+            <div class="">
+
+              <div class="p-2 overflow-y-auto min-h-25 max-h-25">
+                <h2>
+                  Lot for Sale
+                </h2>
+                <p class = "font-thin">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus erat id lacinia convallis. Nunc sapien lectus, laoreet tristique velit id, porttitor pellentesque ante. Curabitur malesuada eleifend velit, sed cursus ligula pretium sit amet. Phasellus rhoncus elit ut accumsan gravida. Sed accumsan mauris eget erat fringilla ullamcorper
+                </p>
+              </div>
+
+              <div>
+                <button command="show-modal" commandfor="dialog" class = " bg-blue-700 hover:bg-orange-400 text-white font-semibold text-xs rounded-full h-5 w-45 mt-4">Click for more information.</button>
+                
+              </div>
+            </div>
+
+            
+
+          </div>
+  )
+
+}
+
+function ImageDisplay(autoplayState, elementDisplay) {
+  const autoplayOptions = { delay: 3000 }; 
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay(autoplayOptions)])
+
+    useEffect(() => {
+    if (!emblaApi) return
+        emblaApi.reInit()
+
+    if(autoplayState)
+      emblaApi.plugins().autoplay?.play()
+    else
+      emblaApi.plugins().autoplay?.stop();
+
+  }, [emblaApi])
+
+
+
+  const goToPrev = () => emblaApi?.scrollPrev()
+  const goToNext = () => emblaApi?.scrollNext()
+
+
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          <div className = "embla__slide">
+            {elementDisplay}
+          </div>
+
+          <div className = "embla__slide">
+            {elementDisplay}
+          </div>
+          <div className = "embla__slide">
+            {elementDisplay}
+          </div>
+            
+        </div>
+      </div>
+
+
+
+       
+    </div>
+  )
+}
+function MiniItemDisplay() {
+  return(
+    <div>
+      <div class="mb-5">
+          <input type="text" class=" border border-indigo-600 focus:outline-hidden rounded-full min-w-full max-w-full min-h-10 max-h-10 px-4 py-2"placeholder='Search'></input>
+      </div>
+      <div class = "grid grid-cols-2 gap-x-25 gap-y-10">
+        <MiniDisplay />
+            <MiniDisplay />
+
+      <MiniDisplay />
+
+      <MiniDisplay />
+
+      <MiniDisplay />
+
+      <MiniDisplay />
+      </div>
+      
+
+    </div>
+  ) 
+}
+
+function ItemDisplay(image) {
+  return (
+    <div class = "flex p-5 ml-10">
+      <div class="min-h-100 max-h-100 min-w-100 max-w-100  mr-7">
+        {ImageDisplay(true, image)}
+
+        <div class="grid grid-cols-2 mt-2">
+          <div>
+            <p>Time Remaining: 6:23:42</p>
+            <br></br>
+            <div>
+              <h3>Current Highest Bid: </h3>
+              <h2 class="text-lime-400">$4,431.00</h2>
+
+              <h3>Minimum Bid: </h3>
+              <h2 class="text-lime-600">$4,600.00</h2>
+            </div>
+          </div>
+          
+          <div class="text-end">
+            <p>Auctioneer:</p>
+            <h2 class="font-thin">Cedrick C. Senados</h2>
+            <br></br>
+
+            <p>Phone Number:</p>
+            <h2 class="font-thin">09662997866</h2>
+          </div>
+          
+        </div>
+      </div>
+
+
+
+      <div class = "grid-rows-2">
+        <div class="overflow-y-auto min-w-100 max-w-100 min-h-60 max-h-60">
+          <h1 class="text-2xl">House and Lot for Sale 165 SQM Fully Titled Near GYM PUROK 1 LA FILIPINA
+            </h1>     
+            <p class="font-thin font-sans"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus erat id lacinia convallis. Nunc sapien lectus, laoreet tristique velit id, porttitor pellentesque ante. Curabitur malesuada eleifend velit, sed cursus ligula pretium sit amet. Phasellus rhoncus elit ut accumsan gravida. Sed accumsan mauris eget erat fringilla ullamcorper. Pellentesque placerat tellus at diam dapibus, quis porttitor nunc elementum. Nam tortor purus, volutpat vel tempus facilisis, ultrices sollicitudin nunc. Nullam purus ligula, fringilla in augue at, volutpat condimentum felis.
+
+Cras scelerisque lobortis rhoncus. Duis neque diam, convallis ut tellus quis, porta malesuada tortor. In fermentum est id magna dapibus, eget porta sem luctus. Sed maximus metus in augue lacinia sagittis nec sit amet ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec quis augue diam. Sed sodales tellus turpis, id interdum risus sagittis a. Nulla luctus libero eu mauris ultricies maximus. Duis scelerisque blandit diam, a viverra tellus faucibus ut. Suspendisse volutpat et metus a euismod.
+
+Vivamus sed augue blandit augue sagittis molestie. Aliquam rhoncus nisl quam, vel imperdiet ex facilisis nec. Quisque tristique placerat diam in laoreet. Maecenas nec orci quam. Duis neque risus, efficitur finibus blandit a, ultrices ac leo. Sed commodo varius dui id fringilla. Duis interdum neque vehicula odio euismod convallis. Praesent quis porttitor mauris. Fusce at eleifend tellus. Proin vestibulum nisi eros, eget fringilla nulla vulputate vel. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus molestie vestibulum lacus, ut posuere tortor. Vestibulum rutrum, sem ac pharetra condimentum, ipsum lorem dignissim leo, vitae pulvinar lectus libero feugiat risus. Praesent ut neque in enim rutrum tincidunt consequat a ante. Sed ultrices finibus nibh, ac porttitor neque elementum in. Fusce laoreet facilisis dolor, elementum hendrerit sapien egestas et. 
+              </p>   
+        </div>
+        
+        <div class="mt-4">
+          <div>
+            <input type="text" class=" border border-indigo-600 focus:border-lime-400 focus:outline-hidden py-2 px-4 rounded-lg min-w-100 max-w-100 "></input>
+          </div>
+          <div>
+            <button class = " bg-blue-700 hover:bg-lime-400 text-white font-bold py-2 px-4 rounded-full min-w-100 max-w-100 mt-5">Place Bid</button>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+
+
+  );
+}
+
+function NoDisplay() {
+  return(
+    <div>
+      <h1>Nothing To Show!</h1>
+    </div>
+  )
+}
+
+function PopupDisplay() {
+
+  
+  return (
+
+
+  <el-dialog>
+  <dialog id="dialog" aria-labelledby="dialog-title" class="fixed inset-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent backdrop:bg-transparent">
+    <el-dialog-backdrop class="fixed inset-0 z-40 bg-gray-950/20 transition-opacity backdrop-blur-xs data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></el-dialog-backdrop>
+
+    <div tabIndex="0" class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+      <el-dialog-panel class="relative z-50 flex justify-between min-w-250 max-w-250 min-h-110 max-h-110 bg-slate-900 border-2 border-gray-300 rounded-xl shadow-md p-6 mt-10 mb-10 text-white">
+        <div class="absolute">
+          <button command="close" commandfor="dialog"  class="size-10 border rounded-full font-sans border-indigo-400 text-cyan-400">X</button>
+        </div>
+        
+        <div>
+        {ItemDisplay(<img src='/src/assets/img1.jpg'></img>)}
+      </div>
+
+      </el-dialog-panel>
+    </div>
+  </dialog>
+</el-dialog>
+    
+  ); 
+
+}
+
+function PromoDisplay() {
+  return (
+    <div class= "flex justify-between min-w-250 max-w-250 min-h-110 max-h-110 bg-slate-900/80 border-2 border-gray-300 rounded-xl shadow-md p-6 mt-10 mb-10 text-white">
+        {/* {ItemDisplay()} */}
+
+        {/* {ItemDisplay(<img src='/src/assets/img1.jpg'></img>)} */}
+        {ImageDisplay(true, ItemDisplay(<img src='/src/assets/img1.jpg'></img>))}
+    </div>
+  );
+}
+
+function MainDisplay() {
+  // let display = (ImageDisplay() == null) ? <NoDisplay /> : <ImageDisplay />;
+  
+  return(
+  
+  <div class= "flex justify-center w-250 h-fit bg-slate-900/80 border-2 border-gray-300 rounded-xl shadow-md p-6 mt-10 mb-10 text-white">
+    <MiniItemDisplay />
+
+  </div>
+
+  );
+    
+}
+
+function App() {
+
+  return (
+    <div class = "font-bold font-display">
+      <div class = "bg-slate-900 h-25 sticky">
+        <header>
+        <div class = "grid grid-cols-2">
+          <div class = "flex items-center justify-center m-6 text-5xl text-yellow-400">
+            <h1>Auction System</h1>
+          </div>
+          
+          <div class = "flex items-center justify-end mr-20">
+            <ul>
+              <div class = "flex justify-between space-x-15 text-orange-500">
+                <li><h3>HOME</h3></li>
+                <li><h3>CREATE</h3></li>
+                <li><h3>ABOUT</h3></li>
+                <li><h3>HELP</h3></li>
+                <li><h3>SETTINGS</h3></li>
+                <li><h3>ACCOUNT</h3></li>
+              </div>
+              
+            </ul>
+
+          </div>
+        </div>
+        
+      </header>
+      </div>
+
+      <div class = "flex justify-center">
+        <div class="grid-cols-1 grid-rows-2">
+          <PromoDisplay />
+          <MainDisplay />
+          <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <PopupDisplay />
+          </div>
+
+        </div>
+        
+        
+      </div>
+    </div>
+
+  );
+}
+
+export default App
